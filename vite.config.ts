d27ca097c68f74fa9cc46a936dfda9345f1c014c
Guilherme Aquino <guiiaquino92@gmail.com>
2022-08-26 13:22:33 -0300

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'https://guiaquinodev.github.io/ignite-feed/'
+  base:'https://guiaquinodev.github.io/ignite-feed/',
+  plugins: [react()]
 })
